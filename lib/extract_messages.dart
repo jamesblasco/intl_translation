56bffd48811569506683c4e1019e0fab336cd2b4
Jaime Blasco <git@jaimeblasco.com>
2020-06-11 09:32:26 +0200

    Separate readAsStringSync from MessageExtraction

--- lib/extract_messages.dart
+++ lib/extract_messages.dart
@@ -82,7 +82,19 @@ class MessageExtraction {
   Map<String, MainMessage> parseFile(File file, [bool transformer = false]) {
     // Optimization to avoid parsing files we're sure don't contain any messages.
     String contents = file.readAsStringSync();
-    origin = file.path;
+    parseFileContent(contents, file.path, transformer);
+  }
+
+  /// Parse the source of the Dart program from a file with content [fileContent] and path [path]
+  /// and return a Map from message names to [IntlMessage] instances.
+  ///
+  /// If [transformer] is true, assume the transformer will supply any "name"
+  /// and "args" parameters required in Intl.message calls.
+  Map<String, MainMessage> parseFileContent(String fileContent, String filepath,
+      [bool transformer = false]) {
+    // Optimization to avoid parsing files we're sure don't contain any messages.
+    String contents = fileContent;
+    origin = filepath;
     if (contents.contains("Intl.")) {
       root = _parseCompilationUnit(contents, origin);
     } else {
@@ -355,8 +367,7 @@ class MessageFindingVisitor extends GeneralizingAstVisitor {
     var extractionResult = extract(message, arguments);
     if (extractionResult == null) return null;
 
-    for (var namedArgument
-        in arguments.whereType<NamedExpression>()) {
+    for (var namedArgument in arguments.whereType<NamedExpression>()) {
       var name = namedArgument.name.label.name;
       var exp = namedArgument.expression;
       var evaluator = new ConstantEvaluator();
